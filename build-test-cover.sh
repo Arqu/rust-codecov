11b76e0c84846edb0ec29145c823270803024a0d
Asmir Avdicevic <asmir.avdicevic64@gmail.com>
2022-11-22 23:06:13 +0000

    fix

--- build-test-cover.sh
+++ build-test-cover.sh
@@ -6,10 +6,15 @@ export LLVM_PROFILE_FILE="target/debug/coverage/%m.profraw"
 # build the test binary with coverage instrumentation
 executables=$(RUSTFLAGS="-Zinstrument-coverage" cargo +nightly test --tests --no-run --message-format=json | jq -r "select(.profile.test == true) | .executable")
 
-echo $executables
+# echo $executables
 
 # run instrumented tests
-$executables
+executables=( $executables )
+for i in "${executables[@]}"
+do
+    echo "Running $i"
+    $i
+done
 
 # combine profraw files
 cargo +nightly profdata -- merge -sparse target/debug/coverage/*.profraw -o target/debug/coverage/combined.profdata
